1bb19e9ec2b8411ef72a719b612547d377fb00fb
belek <belektolubaev@gmail.com>
2019-02-25 18:49:18 +0600

    испраление багов

--- src/components/InfoCountry/InfoCountry.js
+++ src/components/InfoCountry/InfoCountry.js
@@ -40,20 +40,20 @@ class Info extends Component {
     render() {
         return (
             this.state.loadedCountry ? <div>
-                <Jumbotron className="mt-4 mr-4" style={{ height: 580}}>
+                <Jumbotron className="mt-4 mr-4 mb-0" style={{ height: 580, overflowY: 'scroll'}}>
                     <h1 className="text-danger mb-4">{this.state.loadedCountry.name} <img className="float-right" style={{ height: 200, width: 300}} src={this.state.loadedCountry.flag} alt="#"/></h1>
                     <h5 className="text-left">Столица: {this.state.loadedCountry.capital}</h5>
                     <h5 className="mb-5 text-left">Население: {this.state.loadedCountry.population} человек</h5>
                     <div className="text-left">Граничит с:
-                        <Row className="mt-3 ml-5">
+                        <div className="mt-3 ml-5">
                             {this.state.border.map(country =>
                                 <p className="ml-5"> -- {country.name} -- </p>
                             )}
-                        </Row>
+                        </div>
                     </div>
                 </Jumbotron>
             </div> :
-                <Jumbotron className="mt-4 mr-4" style={{ height: 580}}>
+                <Jumbotron className="mt-4 mr-4 mb-0" style={{ height: 580}}>
                     <h2 className="text-warning">Выберите страну чтобы узнать об этой стране по больше</h2>
                 </Jumbotron>
         );
